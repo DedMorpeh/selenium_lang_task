a04ecd76080982caa392802ea753dd4b726582e3
DedMorpeh <tanatosvii@gmail.com>
2020-10-25 18:00:05 +0300

    rename test func

--- task_lang/test_items.py
+++ task_lang/test_items.py
@@ -1,4 +1,4 @@
-def test_add_to_basket_button(browser):
+def test_add_to_basket_button_exists(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     assert bool(browser.find_element_by_css_selector('.btn-add-to-basket'))
